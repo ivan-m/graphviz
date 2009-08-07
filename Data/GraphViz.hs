f9828fcee1b9a25934707e9acf071f88e03d4fc4
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-08-07 18:12:53 +1000

    Replace all graph -> Dot (or ones which do so) with manual and automatic direction version.
    Ignore-this: cd24c09b659cd68f144773ce857cc1ef
    
    darcs-hash:20090807081253-97e08-4113ab2a261eeed7999875aa5a21026555699788

--- Data/GraphViz.hs
+++ Data/GraphViz.hs
@@ -12,16 +12,32 @@
 
    Information about GraphViz and the Dot language can be found at:
    <http://graphviz.org/>
+
+   Commands for converting graphs to Dot format have two options: one
+   in which the user specifies whether the graph is directed or
+   undirected, and a primed version which attempts to automatically
+   infer if the graph is directed or not.  Note that these conversion
+   functions assume that undirected graphs have every edge being
+   duplicated (or at least that if there exists an edge from /n1/ to
+   /n2/, then /n1 <= n2/).
  -}
 
 module Data.GraphViz
-    ( graphToDot
-    , clusterGraphToDot
-    , graphToGraph
-    , dotizeGraph
+    ( -- * Conversion from graphs to /Dot/ format.
+      graphToDot
+    , graphToDot'
+      -- ** Conversion with support for clusters.
     , NodeCluster(..)
+    , clusterGraphToDot
+    , clusterGraphToDot'
+      -- * Passing the graph through GraphViz.
     , AttributeNode
     , AttributeEdge
+    , graphToGraph
+    , graphToGraph'
+    , dotizeGraph
+    , dotizeGraph'
+      -- * Re-exporting other modules.
     , module Data.GraphViz.Types
     , module Data.GraphViz.Attributes
     , module Data.GraphViz.Commands
@@ -43,35 +59,49 @@ import System.IO.Unsafe(unsafePerformIO)
 
 -- -----------------------------------------------------------------------------
 
--- | Determine if the given graph is undirected or directed.
-isUndir   :: (Ord b, Graph g) => g a b -> Bool
-isUndir g = all hasFlip es
+-- | Determine if the given graph is undirected.
+isUndirected   :: (Ord b, Graph g) => g a b -> Bool
+isUndirected g = all hasFlip es
     where
       es = labEdges g
       eSet = Set.fromList es
       hasFlip e = Set.member (flippedEdge e) eSet
       flippedEdge (f,t,l) = (t,f,l)
 
+-- | Determine if the given graph is directed.
+isDirected :: (Ord b, Graph g) => g a b -> Bool
+isDirected = not . isUndirected
+
 -- -----------------------------------------------------------------------------
 
--- | Convert a graph to GraphViz's /Dot/ format.
-graphToDot :: (Ord b, Graph gr) => gr a b -> [Attribute]
-           -> (LNode a -> [Attribute]) -> (LEdge b -> [Attribute]) -> DotGraph
-graphToDot graph gAttributes fmtNode fmtEdge
-    = clusterGraphToDot graph gAttributes clusterBy fmtCluster fmtNode fmtEdge
+-- | Convert a graph to GraphViz's /Dot/ format.  The 'Bool' value is
+--   'True' for directed graphs, 'False' otherwise.
+graphToDot :: (Graph gr) => Bool -> gr a b -> [Attribute]
+              -> (LNode a -> [Attribute]) -> (LEdge b -> [Attribute])
+              -> DotGraph
+graphToDot isDir graph gAttributes fmtNode fmtEdge
+    = clusterGraphToDot isDir graph gAttributes clusterBy fmtCluster fmtNode fmtEdge
       where
         clusterBy :: LNode a -> NodeCluster () a
         clusterBy = N
         fmtCluster _ = []
 
+-- | Convert a graph to GraphViz's /Dot/ format with automatic
+--   direction detection.
+graphToDot'       :: (Ord b, Graph gr) => gr a b -> [Attribute]
+                     -> (LNode a -> [Attribute]) -> (LEdge b -> [Attribute])
+                     -> DotGraph
+graphToDot' graph = graphToDot (isDirected graph) graph
+
 -- | Convert a graph to /Dot/ format, using the specified clustering function
 --   to group nodes into clusters.
 --   Clusters can be nested to arbitrary depth.
-clusterGraphToDot :: (Ord c, Ord b, Graph gr) => gr a b
-                  -> [Attribute] -> (LNode a -> NodeCluster c a)
-                  -> (c -> [Attribute]) -> (LNode a -> [Attribute])
-                  -> (LEdge b -> [Attribute]) -> DotGraph
-clusterGraphToDot graph gAttrs clusterBy fmtCluster fmtNode fmtEdge
+--   The 'Bool' argument is 'True' for directed graphs, 'False' otherwise.
+clusterGraphToDot :: (Ord c, Graph gr) => Bool -> gr a b
+                     -> [Attribute] -> (LNode a -> NodeCluster c a)
+                     -> (c -> [Attribute]) -> (LNode a -> [Attribute])
+                     -> (LEdge b -> [Attribute]) -> DotGraph
+clusterGraphToDot dirGraph graph gAttrs clusterBy fmtCluster fmtNode fmtEdge
     = DotGraph { strictGraph     = False
                , directedGraph   = dirGraph
                , graphID         = Nothing
@@ -80,15 +110,25 @@ clusterGraphToDot graph gAttrs clusterBy fmtCluster fmtNode fmtEdge
                , graphEdges      = es
                }
       where
-        dirGraph = not $ isUndir graph
         ns = clustersToNodes clusterBy fmtCluster fmtNode graph
         es = mapMaybe mkDotEdge . labEdges $ graph
         mkDotEdge e@(f,t,_) = if dirGraph || f <= t
-                              then Just DotEdge {edgeHeadNodeID = f
-                                                ,edgeTailNodeID = t
-                                                ,edgeAttributes = fmtEdge e
-                                                ,directedEdge = dirGraph}
+                              then Just DotEdge { edgeHeadNodeID = f
+                                                , edgeTailNodeID = t
+                                                , edgeAttributes = fmtEdge e
+                                                , directedEdge   = dirGraph
+                                                }
                               else Nothing
+
+-- | Convert a graph to /Dot/ format, using the specified clustering function
+--   to group nodes into clusters.
+--   Clusters can be nested to arbitrary depth.
+--   Graph direction is automatically inferred.
+clusterGraphToDot'       :: (Ord c, Ord b, Graph gr) => gr a b
+                            -> [Attribute] -> (LNode a -> NodeCluster c a)
+                            -> (c -> [Attribute]) -> (LNode a -> [Attribute])
+                            -> (LEdge b -> [Attribute]) -> DotGraph
+clusterGraphToDot' graph = clusterGraphToDot (isDirected graph) graph
 
 -- -----------------------------------------------------------------------------
 
@@ -98,26 +138,29 @@ type AttributeEdge b = ([Attribute], b)
 -- | Run the graph via dot to get positional information and then
 --   combine that information back into the original graph.
 --   Note that this doesn't support graphs with clusters.
-graphToGraph :: (Ord b, Graph gr) => gr a b -> [Attribute]
+--
+--   The 'Bool' argument is 'True' for directed graphs, 'False'
+--   otherwise.  Directed graphs are passed through /dot/, and
+--   undirected graphs through /neato/.
+graphToGraph :: (Graph gr) => Bool -> gr a b -> [Attribute]
                 -> (LNode a -> [Attribute]) -> (LEdge b -> [Attribute])
                 -> IO (gr (AttributeNode a) (AttributeEdge b))
-graphToGraph gr gAttributes fmtNode fmtEdge
+graphToGraph isDir gr gAttributes fmtNode fmtEdge
     = do output <- graphvizWithHandle command dot DotOutput hGetContents
          let res = fromJust output
          length res `seq` return ()
          return $ rebuildGraphWithAttributes res
     where
-      undirected = isUndir gr
-      command = if undirected then undirCommand else dirCommand
-      dot = graphToDot gr gAttributes fmtNode fmtEdge
+      command = if isDir then dirCommand else undirCommand
+      dot = graphToDot isDir gr gAttributes fmtNode fmtEdge
       rebuildGraphWithAttributes dotResult = mkGraph lnodes ledges
           where
             lnodes = map (\(n, l) -> (n, (fromJust $ Map.lookup n nodeMap, l)))
                      $ labNodes gr
             ledges = map createEdges $ labEdges gr
-            createEdges (f, t, l) = if undirected && f > t
-                                    then (f, t, getLabel (t,f))
-                                    else (f, t, getLabel (f,t))
+            createEdges (f, t, l) = if isDir || f <= t
+                                    then (f, t, getLabel (f,t))
+                                    else (f, t, getLabel (t,f))
                 where
                   getLabel c = (fromJust $ Map.lookup c edgeMap, l)
             DotGraph { graphNodes = ns, graphEdges = es}
@@ -128,13 +171,36 @@ graphToGraph gr gAttributes fmtNode fmtEdge
                                                 , edgeAttributes e)
                                          ) es
 
+
+-- | Run the graph via dot to get positional information and then
+--   combine that information back into the original graph.
+--   Note that this doesn't support graphs with clusters.
+--   Graph direction is automatically inferred.
+graphToGraph'    :: (Ord b, Graph gr) => gr a b -> [Attribute]
+                    -> (LNode a -> [Attribute]) -> (LEdge b -> [Attribute])
+                    -> IO (gr (AttributeNode a) (AttributeEdge b))
+graphToGraph' gr = graphToGraph (isDirected gr) gr
+
 -- | Pass the plain graph through 'graphToGraph'.  This is an @IO@ action,
 --   however since the state doesn't change it's safe to use 'unsafePerformIO'
 --   to convert this to a normal function.
-dotizeGraph   :: (DynGraph gr, Ord b) => gr a b
-              -> gr (AttributeNode a) (AttributeEdge b)
-dotizeGraph g = unsafePerformIO
-                $ graphToGraph g gAttrs noAttrs noAttrs
+--
+--   The 'Bool' argument is 'True' for directed graphs, 'False'
+--   otherwise.  Directed graphs are passed through /dot/, and
+--   undirected graphs through /neato/.
+dotizeGraph         :: (DynGraph gr) => Bool -> gr a b
+                       -> gr (AttributeNode a) (AttributeEdge b)
+dotizeGraph isDir g = unsafePerformIO
+                      $ graphToGraph isDir g gAttrs noAttrs noAttrs
     where
       gAttrs = []
       noAttrs = const []
+
+
+-- | Pass the plain graph through 'graphToGraph'.  This is an @IO@ action,
+--   however since the state doesn't change it's safe to use 'unsafePerformIO'
+--   to convert this to a normal function.
+--   The graph direction is automatically inferred.
+dotizeGraph'   :: (DynGraph gr, Ord b) => gr a b
+                  -> gr (AttributeNode a) (AttributeEdge b)
+dotizeGraph' g = dotizeGraph (isDirected g) g
