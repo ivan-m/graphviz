371442efe31839793e857b7e7239760ff374dcec
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-11-23 01:19:56 +1100

    Clean up Properties and add more properties
    Ignore-this: 69fc6f79ecee45b3e75d1ff0e9d17fa1
    
    darcs-hash:20091122141956-97e08-4a8c576fd6287015241d4a4a745bb4b9f48ee648

--- Data/GraphViz/Testing/Properties.hs
+++ Data/GraphViz/Testing/Properties.hs
@@ -1,11 +1,18 @@
+{- |
+   Module      : Data.GraphViz.Testing.Properties
+   Description : Properties for testing.
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : 3-Clause BSD-style
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   Various properties that should hold true for the graphviz library.
+-}
 module Data.GraphViz.Testing.Properties where
 
+import Data.GraphViz(prettyPrint')
+import Data.GraphViz.Types(DotGraph, printDotGraph)
 import Data.GraphViz.Types.Printing(PrintDot(..), printIt)
-import Data.GraphViz.Types.Parsing( ParseDot(..), parseIt
-                                  , quoteChar, isIntString, isNumString)
-
-import Data.GraphViz.Attributes
-import Data.GraphViz.Types
+import Data.GraphViz.Types.Parsing(ParseDot(..), parseIt, preProcess)
 
 import Test.QuickCheck
 
@@ -19,13 +26,34 @@ import Data.Word(Word8)
 
 -- | Checking that @parse . print == id@; that is, graphviz can parse
 --   its own output.
-printParse_prop = id
+prop_printParseID   :: (ParseDot a, PrintDot a, Eq a) => a -> Bool
+prop_printParseID a = fst (tryParse a) == a
 
-printParse   :: (ParseDot a, PrintDot a, Eq a) => a -> Bool
-printParse a = fst (tryParse a) == a
+-- | A version of 'prop_printParse' specifically for lists; it ensures
+--   that the list is not empty (as most list-based parsers fail on
+--   empty lists).
+prop_printParseListID    :: (ParseDot a, PrintDot a, Eq a) => [a] -> Property
+prop_printParseListID as =  not (null as) ==> prop_printParseID as
 
-printParseList    :: (ParseDot a, PrintDot a, Eq a) => [a] -> Property
-printParseList as =  not (null as) ==> printParse as
+-- | Pre-processing shouldn't change the output of printed Dot code.
+--   This should work for all 'PrintDot' instances, but is more
+--   specific to 'DotGraph' values.
+prop_preProcessingID    :: (PrintDot a) => DotGraph a -> Bool
+prop_preProcessingID dg = preProcess dotCode == dotCode
+  where
+    dotCode = printDotGraph dg
 
+-- | This is a version of 'prop_printParseID' that tries to parse the
+-- | pretty-printed output of 'prettyPrint'' rather than just 'printIt'.
+prop_parsePrettyID    :: (PrintDot a) => DotGraph a -> Bool
+prop_parsePrettyID dg = (fst . parseIt . prettyPrint') dg == dg
+
+-- -----------------------------------------------------------------------------
+-- Helper utility functions
+
+-- | A utility function to use for debugging purposes for trying to
+--   find how graphviz /is/ parsing something.  This is easier than
+--   using @'parseIt' . 'printIt'@ directly, since it avoids having to
+--   enter and explicit type signature.
 tryParse :: (ParseDot a, PrintDot a) => a -> (a, String)
 tryParse = parseIt . printIt
