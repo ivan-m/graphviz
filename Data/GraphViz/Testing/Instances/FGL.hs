6a92c7b635cb77c32ab052d95f25c5533af7929c
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-11-23 15:00:19 +1100

    Add the FGL instance module
    Ignore-this: fe4a90bba982ef06456332760ec852d6
    Has to be in a separate module to avoid overlapping instances problems
    due to how FGL graphs are defined (same reason they don't have Eq instances).
    
    darcs-hash:20091123040019-97e08-6a326cb9e2abeef0a49e603456d59b28bedc9a25

---
+++ Data/GraphViz/Testing/Instances/FGL.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+{- |
+   Module      : Data.GraphViz.Testing.Instances.FGL
+   Description : 'Arbitrary' instances for FGL graphs.
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : 3-Clause BSD-style
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   This module defines the 'Arbitrary' instances for FGL 'DynGraph'
+   graphs.  Note that this instance cannot be in
+   "Data.GraphViz.Testing.Instances", as this instance requires the
+   FlexibleInstances extension, which makes some of the other
+   'Arbitrary' instances fail to type-check.
+-}
+module Data.GraphViz.Testing.Instances.FGL() where
+
+import Test.QuickCheck
+
+import Data.Graph.Inductive.Graph( Graph, mkGraph, nodes, delNode)
+import Data.List(nub)
+import Control.Monad(liftM, liftM3)
+
+-- -----------------------------------------------------------------------------
+-- Arbitrary instance for FGL graphs.
+
+instance (Graph g, Arbitrary n, Arbitrary e) => Arbitrary (g n e) where
+  arbitrary = do ns <- liftM nub arbitrary
+                 let nGen = elements ns
+                 lns <- mapM makeLNode ns
+                 les <- listOf $ makeLEdge nGen
+                 return $ mkGraph lns les
+    where
+      genCon nGen n = ()
+      makeLNode n = liftM ((,) n) arbitrary
+      makeLEdge nGen = liftM3 (,,) nGen nGen arbitrary
+
+  shrink gr = map (flip delNode gr) (nodes gr)
