f1597f6523d60731c1c87521d0eb4058acc940d3
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-11-24 00:23:55 +1100

    Add the RunTests module
    Ignore-this: c9b75e1ee6e513d137f13e62efd298a8
    This just sets main = runDefaultTests from Testing
    
    darcs-hash:20091123132355-97e08-543dea990a41092fe746b1d47150c301f502b140

---
+++ RunTests.hs
@@ -0,0 +1,16 @@
+{- |
+   Module      : RunTests
+   Description : Run the graphviz test suite.
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : 3-Clause BSD-style
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   This module exists solely to make a Main module to build and run
+   the test suite.
+-}
+module Main where
+
+import Data.GraphViz.Testing
+
+main :: IO ()
+main = runDefaultTests
