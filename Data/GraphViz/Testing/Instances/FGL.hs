f3907e70c00e7507884a91d8e1d98fd7d243c658
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-11-26 00:51:36 +1100

    Clean up the FGL Arbitrary instance
    Ignore-this: 4f2a5b9a9ba4173762bb3665e9c558fc
    * Make sure the list of nodes is non-empty
    * Remove multiple edges (that is, have at most one edge from f -> t).
    
    darcs-hash:20091125135136-97e08-1662c951c921bb9e2956fcea2ca22f74c0669a4e

--- Data/GraphViz/Testing/Instances/FGL.hs
+++ Data/GraphViz/Testing/Instances/FGL.hs
@@ -19,19 +19,23 @@ module Data.GraphViz.Testing.Instances.FGL() where
 import Test.QuickCheck
 
 import Data.Graph.Inductive.Graph(Graph, mkGraph, nodes, delNode)
-import Data.List(nub)
+import Data.Function(on)
+import Data.List(group, sort, nubBy)
 import Control.Monad(liftM, liftM3)
 
 -- -----------------------------------------------------------------------------
 -- Arbitrary instance for FGL graphs.
 
 instance (Graph g, Arbitrary n, Arbitrary e) => Arbitrary (g n e) where
-  arbitrary = do ns <- liftM nub arbitrary
+  arbitrary = do ns <- suchThat genNs (not . null)
                  let nGen = elements ns
                  lns <- mapM makeLNode ns
-                 les <- listOf $ makeLEdge nGen
+                 les <- liftM filtEs . listOf $ makeLEdge nGen
                  return $ mkGraph lns les
     where
+      genNs = liftM (map head . group . sort) arbitrary
+      filtEs = nubBy ((==) `on` toE)
+      toE (f,t,_) = (f,t)
       makeLNode n = liftM ((,) n) arbitrary
       makeLEdge nGen = liftM3 (,,) nGen nGen arbitrary
 
