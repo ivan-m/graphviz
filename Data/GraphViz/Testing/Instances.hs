8edb2a77b11740b543de4100ef80e0da41b995e4
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2009-11-26 21:53:11 +1100

    Make sure that even shrinking a String doesn't create a Bool
    Ignore-this: ab4b3780d05eaf6662338d986f041759
    
    darcs-hash:20091126105311-97e08-6043cad0721c9d6595383cd447dee495261bf83f

--- Data/GraphViz/Testing/Instances.hs
+++ Data/GraphViz/Testing/Instances.hs
@@ -765,17 +765,18 @@ posArbitrary :: (Arbitrary a, Num a, Ord a) => Gen a
 posArbitrary = liftM fromPositive arbitrary
 
 arbString :: Gen String
-arbString = suchThat (liftM (map toLower) genStr) isValid
+arbString = suchThat (liftM (map toLower) genStr) validString
   where
     genStr = listOf1 $ elements strChr
     strChr = ['a'..'z'] ++ ['0'..'9'] ++ ['\'', '"', ' ', '\t', '(', ')'
                                          , ',', ':', '.']
-    isValid "true"  = False
-    isValid "false" = False
-    isValid str     = notNumStr str
+validString         :: String -> Bool
+validString "true"  = False
+validString "false" = False
+validString str     = notNumStr str
 
 shrinkString :: String -> [String]
-shrinkString = filter notNumStr . nonEmptyShrinks
+shrinkString = filter validString . nonEmptyShrinks
 
 notNumStr :: String -> Bool
 notNumStr = not . isNumString
