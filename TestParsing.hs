ddd319aa41961da532947ed907ff66685761ea04
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2010-04-06 23:16:50 +1000

    Add TestParsing.hs
    Ignore-this: ba77f62836ff1a382bd9accc0b5a5e25
    This is not yet listed in the .cabal file; want to sort out the Cabal
    1.8 feature of having executable files depend on the library they ship
    with first to avoid having to build the library three times :s
    
    darcs-hash:20100406131650-97e08-713ff75445e78ea3efcdcef77f2a4a125a4cc985

---
+++ TestParsing.hs
@@ -0,0 +1,78 @@
+{- |
+   Module      : TestParsing
+   Description : Check if the graphviz parser can parse "real world" Dot code.
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : 3-Clause BSD-style
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   This module defines a program that determines if the provided files
+   containing Dot code can be properly parsed using graphviz's parsers
+   (with the assumption that the provided code is valid).
+
+-}
+module Main where
+
+import Data.GraphViz
+import Data.GraphViz.Types.Generalised
+import Data.GraphViz.Parsing(runParser, parse, discard, allWhitespace', eof)
+import Data.GraphViz.PreProcessing(preProcess)
+
+import Data.Either(either)
+import Control.Exception(try, ErrorCall(..))
+import Control.Monad(liftM)
+import System.Environment(getArgs)
+
+-- -----------------------------------------------------------------------------
+
+main :: IO ()
+main = tryParsing =<< getArgs
+  where
+    tryParsing [] = putStrLn "Test that the graphviz library can parse\
+                             \ \"real life\" Dot code by passing a list\n\
+                             \of files in which contain Dot graphs."
+    tryParsing fs = mapM_ tryParseFile fs
+
+-- -----------------------------------------------------------------------------
+
+
+withParse :: (DotRepr dg n) => (a -> IO String) -> (dg n -> IO ())
+             -> (ErrMsg -> String) -> a -> IO ()
+withParse toStr withDG cmbErr a = do dc <- toStr a
+                                     edg <- tryParse dc
+                                     case edg of
+                                       (Right dg) -> withDG dg
+                                       (Left err) -> putStrLn $ cmbErr err
+
+type DG = DotGraph String
+type GDG = GDotGraph String
+type ErrMsg = String
+
+tryParseFile    :: FilePath -> IO ()
+tryParseFile fp = withParse readFile
+                            (tryParseCanon fp)
+                            (\ e -> fp ++ ": Cannot parse as a GDotGraph:\n"
+                                    ++ e)
+                            fp
+
+tryParseCanon    :: FilePath -> GDG -> IO ()
+tryParseCanon fp = withParse prettyPrint
+                             (const (return ()) . asDG)
+                             (\ e -> fp ++ ": Canonical Form not a DotGraph:\n"
+                                     ++ e)
+  where
+    asDG = flip asTypeOf emptDG
+    emptDG = DotGraph False False Nothing $ DotStmts [] [] [] [] :: DG
+
+tryParse    :: (DotRepr dg n) => String -> IO (Either ErrMsg (dg n))
+tryParse dc = liftM getErrMsg . try
+              $ let (dg, rst) = runParser p $ preProcess dc
+                in length rst `seq` return dg
+  where
+    p = parse `discard` (allWhitespace' >> eof)
+
+getErrMsg :: Either ErrorCall a -> Either ErrMsg a
+getErrMsg = either getEC Right
+  where
+    getEC (ErrorCall e) = Left e
+
+
