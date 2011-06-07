ef69b1dcd1e168c10dc510fc07edc83677d1dfad
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2011-06-07 21:57:29 +1000

    Shift canonical DotGraph to its own module
    Ignore-this: 1ffe18f8e28002e7c9c96956f1fa0d54
    
    darcs-hash:20110607115729-97e08-e5f1729a282f0de2dcd5f32bd0ed0697a0ae8145

--- Data/GraphViz/Types.hs
+++ Data/GraphViz/Types.hs
@@ -1,18 +1,16 @@
-{-# LANGUAGE   MultiParamTypeClasses
-             , FlexibleInstances
-  #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 
 {- |
    Module      : Data.GraphViz.Types
-   Description : Definition of the Graphviz types.
-   Copyright   : (c) Matthew Sackman, Ivan Lazar Miljenovic
+   Description : Haskell representation of Dot graphs.
+   Copyright   : (c) Ivan Lazar Miljenovic
    License     : 3-Clause BSD-style
    Maintainer  : Ivan.Miljenovic@gmail.com
 
-   This module defines the overall types and methods that interact
-   with them for the Graphviz library.  The specifications are based
-   loosely upon the information available at:
+   Various different representations of Dot graphs are available, all
+   of which are based loosely upon the specifications at:
      <http://graphviz.org/doc/info/lang.html>
+   The 'DotRepr' class provides a common interface for them.
 
    Printing of /Dot/ code is done as strictly as possible, whilst
    parsing is as permissive as possible.  For example, if the types
@@ -21,28 +19,29 @@
 
    A summary of known limitations\/differences:
 
-   * When creating 'GraphID' values for 'graphID' and 'subGraphID',
+   * When creating 'GraphID' values for graphs and sub-graphs,
      you should ensure that none of them have the same printed value
-     as one of the 'nodeID' values to avoid any possible problems.
+     as one of the node identifiers values to avoid any possible problems.
 
-   * If you want any 'GlobalAttributes' in a 'DotSubGraph' and want
-     them to only apply to that 'DotSubGraph', then you must ensure it
+   * If you want any 'GlobalAttributes' in a sub-graph and want
+     them to only apply to that sub-graph, then you must ensure it
      does indeed have a valid 'GraphID'.
 
-   * All 'DotSubGraph's with @'isCluster' = 'True'@ /must/ have unique
-     'subGraphID' values (well, only if you want them to be generated
+   * All sub-graphs which represent clusters should have unique
+     identifiers (well, only if you want them to be generated
      sensibly).
 
    * If eventually outputting to a format such as SVG, then you should
-     make sure that your 'DotGraph' has a 'graphID', as that is used
-     as the title of the resulting image.
+     make sure to specify an identifier for the overall graph, as that is
+     used as the title of the resulting image.
 
-   * Whilst 'DotGraph', etc. are polymorphic in their node type, you
+   * Whilst the graphs, etc. are polymorphic in their node type, you
      should ensure that you use a relatively simple node type (that
      is, it only covers a single line, etc.).
 
    * Also, whilst Graphviz allows you to mix the types used for nodes,
-     this library requires\/assumes that they are all the same type.
+     this library requires\/assumes that they are all the same type (but
+     you /can/ use a sum-type).
 
    * 'DotEdge' defines an edge @(a, b)@ (with an edge going from @a@
      to @b@); in /Dot/ parlance the edge has a head at @a@ and a tail
@@ -58,13 +57,6 @@
    * It is not yet possible to create or parse edges with
      subgraphs\/clusters as one of the end points.
 
-   * Cannot place items in an arbitrary order: in particular, this
-     means that it is not possible to use the normal Graphviz hack of
-     having graph attributes that do not apply to subgraphs\/clusters by
-     listing them /after/ the subgraphs\/clusters.  If you wish to be able
-     to use an arbitrary ordering, you may wish to use
-     "Data.GraphViz.Types.Generalised".
-
    * The parser will strip out comments and pre-processor lines, join
      together multiline statements and concatenate split strings together.
      However, pre-processing within HTML-like labels is currently not
@@ -74,9 +66,8 @@
      node definition, and then in a subgraph with extra global attributes
      applied to it).  This actually represents the same node, but when
      parsing they will be considered as separate 'DotNode's (such that
-     'graphNodes' will return both \"definitions\").  The canonical form
-     (see @prettyPrint@ in "Data.GraphViz") combines the \"definitions\"
-     into one.
+     'graphNodes' will return both \"definitions\").  @canonicalise@ from
+     "Data.GraphViz.Algorithms" can be used to fix this.
 
    See "Data.GraphViz.Attributes" for more limitations.
 
@@ -94,39 +85,27 @@ module Data.GraphViz.Types
       -- ** Printing and parsing a @DotRepr@.
     , printDotGraph
     , parseDotGraph
-      -- * The overall representation of a graph in /Dot/ format.
-    , DotGraph(..)
-      -- ** Reporting of errors in a @DotGraph@.
-    , DotError(..)
-    , isValidGraph
-    , graphErrors
-      -- * Sub-components of a @DotGraph@.
-    , GraphID(..) -- Re-exported from Data.GraphViz.Types.Common
-    , DotStatements(..)
-    , GlobalAttributes(..) -- Re-exported from Data.GraphViz.Types.Common
-    , DotSubGraph(..)
-    , DotNode(..)
-    , DotEdge(..) -- Re-exported from Data.GraphViz.Types.Common
     ) where
 
-import Data.GraphViz.Types.Common
+import Data.GraphViz.Types.Canonical
+import Data.GraphViz.Types.Common(GraphID, GlobalAttributes, DotNode, DotEdge)
 import Data.GraphViz.Types.State
-import Data.GraphViz.Attributes( Attribute
-                               , usedByGraphs, usedByClusters, usedBySubGraphs)
-import Data.GraphViz.Util
-import Data.GraphViz.Parsing
-import Data.GraphViz.PreProcessing
-import Data.GraphViz.Printing
+import Data.GraphViz.Util(bool)
+import Data.GraphViz.Parsing(ParseDot, parseIt)
+import Data.GraphViz.PreProcessing(preProcess)
+import Data.GraphViz.Printing(PrintDot, printIt)
 
 import Data.Text.Lazy(Text)
-import Control.Arrow((&&&))
-import Control.Monad(liftM)
 
 -- -----------------------------------------------------------------------------
 
 -- | This class is used to provide a common interface to different
 --   ways of representing a graph in /Dot/ form.
 class (PrintDot (dg n), ParseDot (dg n)) => DotRepr dg n where
+  -- | Convert from a graph in canonical form.  This is especially
+  --   useful when using the functions from "Data.GraphViz.Algorithms".
+  fromCanonical :: DotGraph n -> dg n
+
   -- | Return the ID of the graph.
   getID :: dg n -> Maybe GraphID
 
@@ -186,16 +165,11 @@ parseDotGraph :: (DotRepr dg n) => Text -> dg n
 parseDotGraph = fst . parseIt . preProcess
 
 -- -----------------------------------------------------------------------------
-
--- | The internal representation of a graph in Dot form.
-data DotGraph a = DotGraph { strictGraph     :: Bool  -- ^ If 'True', no multiple edges are drawn.
-                           , directedGraph   :: Bool
-                           , graphID         :: Maybe GraphID
-                           , graphStatements :: DotStatements a
-                           }
-                deriving (Eq, Ord, Show, Read)
+-- Instance for Canonical graphs, to avoid cyclic modules.
 
 instance (Ord n, PrintDot n, ParseDot n) => DotRepr DotGraph n where
+  fromCanonical = id
+
   getID = graphID
 
   setID i g = g { graphID = Just i }
@@ -217,73 +191,6 @@ instance (Ord n, PrintDot n, ParseDot n) => DotRepr DotGraph n where
   edgeInformation wGlobal = getDotEdges wGlobal
                             . statementEdges . graphStatements
 
--- | Check if all the 'Attribute's are being used correctly.
-isValidGraph :: DotGraph a -> Bool
-isValidGraph = null . graphErrors
-
--- | Return detectable errors in the 'DotGraph'.
-graphErrors :: DotGraph a -> [DotError a]
-graphErrors = invalidStmts usedByGraphs . graphStatements
-
-instance (PrintDot a) => PrintDot (DotGraph a) where
-  unqtDot = printStmtBased printGraphID' graphStatements toDot
-    where
-      printGraphID' = printGraphID strictGraph directedGraph graphID
-
-instance (ParseDot a) => ParseDot (DotGraph a) where
-  parseUnqt = parseStmtBased parse (parseGraphID DotGraph)
-
-  parse = parseUnqt -- Don't want the option of quoting
-          `adjustErr`
-          (++ "\n\nNot a valid DotGraph")
-
-instance Functor DotGraph where
-  fmap f g = g { graphStatements = fmap f $ graphStatements g }
-
--- -----------------------------------------------------------------------------
-
-data DotStatements a = DotStmts { attrStmts :: [GlobalAttributes]
-                                , subGraphs :: [DotSubGraph a]
-                                , nodeStmts :: [DotNode a]
-                                , edgeStmts :: [DotEdge a]
-                                }
-                     deriving (Eq, Ord, Show, Read)
-
-instance (PrintDot a) => PrintDot (DotStatements a) where
-  unqtDot stmts = vcat $ sequence [ unqtDot $ attrStmts stmts
-                                  , unqtDot $ subGraphs stmts
-                                  , unqtDot $ nodeStmts stmts
-                                  , unqtDot $ edgeStmts stmts
-                                  ]
-
-instance (ParseDot a) => ParseDot (DotStatements a) where
-  parseUnqt = do atts <- tryParseList
-                 newline'
-                 sGraphs <- tryParseList
-                 newline'
-                 nodes <- tryParseList
-                 newline'
-                 edges <- tryParseList
-                 return $ DotStmts atts sGraphs nodes edges
-
-  parse = parseUnqt -- Don't want the option of quoting
-          `adjustErr`
-          (++ "Not a valid set of statements")
-
-instance Functor DotStatements where
-  fmap f stmts = stmts { subGraphs = map (fmap f) $ subGraphs stmts
-                       , nodeStmts = map (fmap f) $ nodeStmts stmts
-                       , edgeStmts = map (fmap f) $ edgeStmts stmts
-                       }
-
--- | The function represents which function to use to check the
---   'GraphAttrs' values.
-invalidStmts         :: (Attribute -> Bool) -> DotStatements a -> [DotError a]
-invalidStmts f stmts = concatMap (invalidGlobal f) (attrStmts stmts)
-                       ++ concatMap invalidSubGraph (subGraphs stmts)
-                       ++ concatMap invalidNode (nodeStmts stmts)
-                       ++ concatMap invalidEdge (edgeStmts stmts)
-
 statementStructure :: DotStatements n -> GraphState ()
 statementStructure stmts
   = do mapM_ addGraphGlobals $ attrStmts stmts
@@ -301,46 +208,6 @@ statementEdges stmts
   = do mapM_ addEdgeGlobals $ attrStmts stmts
        mapM_ (withSubGraphID recursiveCall statementEdges) $ subGraphs stmts
        mapM_ addEdge $ edgeStmts stmts
-
--- -----------------------------------------------------------------------------
-
-data DotSubGraph a = DotSG { isCluster     :: Bool
-                           , subGraphID    :: Maybe GraphID
-                           , subGraphStmts :: DotStatements a
-                           }
-                   deriving (Eq, Ord, Show, Read)
-
-instance (PrintDot a) => PrintDot (DotSubGraph a) where
-  unqtDot = printStmtBased printSubGraphID' subGraphStmts toDot
-
-  unqtListToDot = printStmtBasedList printSubGraphID' subGraphStmts toDot
-
-  listToDot = unqtListToDot
-
-printSubGraphID' :: DotSubGraph a -> DotCode
-printSubGraphID' = printSubGraphID (isCluster &&& subGraphID)
-
-instance (ParseDot a) => ParseDot (DotSubGraph a) where
-  parseUnqt = parseStmtBased parseUnqt (parseSubGraphID DotSG)
-              `onFail`
-              -- Take "anonymous" DotSubGraphs into account.
-              liftM (DotSG False Nothing) (parseBracesBased parseUnqt)
-
-  parse = parseUnqt -- Don't want the option of quoting
-          `adjustErr`
-          (++ "\n\nNot a valid Sub Graph")
-
-  parseUnqtList = sepBy (whitespace' >> parseUnqt) newline'
-
-  parseList = parseUnqtList
-
-instance Functor DotSubGraph where
-  fmap f sg = sg { subGraphStmts = fmap f $ subGraphStmts sg }
-
-invalidSubGraph    :: DotSubGraph a -> [DotError a]
-invalidSubGraph sg = invalidStmts valFunc (subGraphStmts sg)
-  where
-    valFunc = bool usedBySubGraphs usedByClusters (isCluster sg)
 
 withSubGraphID        :: (Maybe (Maybe GraphID) -> b -> a)
                          -> (DotStatements n -> b) -> DotSubGraph n -> a
---
+++ Data/GraphViz/Types/Canonical.hs
@@ -0,0 +1,163 @@
+{- |
+   Module      : Data.GraphViz.Types.Canonical
+   Description : The canonical representation of Dot graphs.
+   Copyright   : (c) Matthew Sackman, Ivan Lazar Miljenovic
+   License     : 3-Clause BSD-style
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+   A canonical Dot graph requires that within each graph/sub-graph,
+   the statements are in the following order:
+
+   * global attributes
+
+   * sub-graphs/clusters
+
+   * nodes
+
+   * edges
+
+   If you require arbitrary ordering of statements, then use
+   "Data.GraphViz.Types.Generalised".
+
+ -}
+module Data.GraphViz.Types.Canonical
+    ( DotGraph(..)
+      -- ** Reporting of errors in a @DotGraph@.
+    , DotError(..)
+    , isValidGraph
+    , graphErrors
+      -- * Sub-components of a @DotGraph@.
+    , GraphID(..) -- Re-exported from Data.GraphViz.Types.Common
+    , DotStatements(..)
+    , GlobalAttributes(..) -- Re-exported from Data.GraphViz.Types.Common
+    , DotSubGraph(..)
+    , DotNode(..) -- Re-exported from Data.GraphViz.Types.Common
+    , DotEdge(..) -- Re-exported from Data.GraphViz.Types.Common
+    ) where
+
+import Data.GraphViz.Types.Common
+import Data.GraphViz.Attributes( Attribute
+                               , usedByGraphs, usedByClusters, usedBySubGraphs)
+import Data.GraphViz.Util(bool)
+import Data.GraphViz.Parsing
+import Data.GraphViz.Printing
+
+import Control.Arrow((&&&))
+import Control.Monad(liftM)
+
+-- -----------------------------------------------------------------------------
+
+-- | A Dot graph in canonical form.
+data DotGraph a = DotGraph { strictGraph     :: Bool  -- ^ If 'True', no multiple edges are drawn.
+                           , directedGraph   :: Bool
+                           , graphID         :: Maybe GraphID
+                           , graphStatements :: DotStatements a
+                           }
+                deriving (Eq, Ord, Show, Read)
+
+-- | Check if all the 'Attribute's are being used correctly.
+isValidGraph :: DotGraph a -> Bool
+isValidGraph = null . graphErrors
+
+-- | Return detectable errors in the 'DotGraph'.
+graphErrors :: DotGraph a -> [DotError a]
+graphErrors = invalidStmts usedByGraphs . graphStatements
+
+instance (PrintDot a) => PrintDot (DotGraph a) where
+  unqtDot = printStmtBased printGraphID' graphStatements toDot
+    where
+      printGraphID' = printGraphID strictGraph directedGraph graphID
+
+instance (ParseDot a) => ParseDot (DotGraph a) where
+  parseUnqt = parseStmtBased parse (parseGraphID DotGraph)
+
+  parse = parseUnqt -- Don't want the option of quoting
+          `adjustErr`
+          (++ "\n\nNot a valid DotGraph")
+
+instance Functor DotGraph where
+  fmap f g = g { graphStatements = fmap f $ graphStatements g }
+
+-- -----------------------------------------------------------------------------
+
+data DotStatements a = DotStmts { attrStmts :: [GlobalAttributes]
+                                , subGraphs :: [DotSubGraph a]
+                                , nodeStmts :: [DotNode a]
+                                , edgeStmts :: [DotEdge a]
+                                }
+                     deriving (Eq, Ord, Show, Read)
+
+instance (PrintDot a) => PrintDot (DotStatements a) where
+  unqtDot stmts = vcat $ sequence [ unqtDot $ attrStmts stmts
+                                  , unqtDot $ subGraphs stmts
+                                  , unqtDot $ nodeStmts stmts
+                                  , unqtDot $ edgeStmts stmts
+                                  ]
+
+instance (ParseDot a) => ParseDot (DotStatements a) where
+  parseUnqt = do atts <- tryParseList
+                 newline'
+                 sGraphs <- tryParseList
+                 newline'
+                 nodes <- tryParseList
+                 newline'
+                 edges <- tryParseList
+                 return $ DotStmts atts sGraphs nodes edges
+
+  parse = parseUnqt -- Don't want the option of quoting
+          `adjustErr`
+          (++ "Not a valid set of statements")
+
+instance Functor DotStatements where
+  fmap f stmts = stmts { subGraphs = map (fmap f) $ subGraphs stmts
+                       , nodeStmts = map (fmap f) $ nodeStmts stmts
+                       , edgeStmts = map (fmap f) $ edgeStmts stmts
+                       }
+
+-- | The function represents which function to use to check the
+--   'GraphAttrs' values.
+invalidStmts         :: (Attribute -> Bool) -> DotStatements a -> [DotError a]
+invalidStmts f stmts = concatMap (invalidGlobal f) (attrStmts stmts)
+                       ++ concatMap invalidSubGraph (subGraphs stmts)
+                       ++ concatMap invalidNode (nodeStmts stmts)
+                       ++ concatMap invalidEdge (edgeStmts stmts)
+
+-- -----------------------------------------------------------------------------
+
+data DotSubGraph a = DotSG { isCluster     :: Bool
+                           , subGraphID    :: Maybe GraphID
+                           , subGraphStmts :: DotStatements a
+                           }
+                   deriving (Eq, Ord, Show, Read)
+
+instance (PrintDot a) => PrintDot (DotSubGraph a) where
+  unqtDot = printStmtBased printSubGraphID' subGraphStmts toDot
+
+  unqtListToDot = printStmtBasedList printSubGraphID' subGraphStmts toDot
+
+  listToDot = unqtListToDot
+
+printSubGraphID' :: DotSubGraph a -> DotCode
+printSubGraphID' = printSubGraphID (isCluster &&& subGraphID)
+
+instance (ParseDot a) => ParseDot (DotSubGraph a) where
+  parseUnqt = parseStmtBased parseUnqt (parseSubGraphID DotSG)
+              `onFail`
+              -- Take "anonymous" DotSubGraphs into account.
+              liftM (DotSG False Nothing) (parseBracesBased parseUnqt)
+
+  parse = parseUnqt -- Don't want the option of quoting
+          `adjustErr`
+          (++ "\n\nNot a valid Sub Graph")
+
+  parseUnqtList = sepBy (whitespace' >> parseUnqt) newline'
+
+  parseList = parseUnqtList
+
+instance Functor DotSubGraph where
+  fmap f sg = sg { subGraphStmts = fmap f $ subGraphStmts sg }
+
+invalidSubGraph    :: DotSubGraph a -> [DotError a]
+invalidSubGraph sg = invalidStmts valFunc (subGraphStmts sg)
+  where
+    valFunc = bool usedBySubGraphs usedByClusters (isCluster sg)
