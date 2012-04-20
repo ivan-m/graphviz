5716a95f6fc34d31ae792f8218a75045bb6b274f
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2012-04-20 15:21:31 +1000

    Need TypeSynonymInstances in Types
    Ignore-this: a401078219c61740e70acb8df255001c
    
    darcs-hash:20120420052131-9c7ab-be1ab737312b3e66f429d5ed935fe199c29008e2

--- Data/GraphViz/Types.hs
+++ Data/GraphViz/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeSynonymInstances #-}
 
 {- |
    Module      : Data.GraphViz.Types
